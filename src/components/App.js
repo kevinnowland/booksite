6c9f32c5492efa6eab59c9bcd0a92dc491c7487c
Kevin Nowland <kevin.nowland@gmail.com>
2022-12-11 21:14:59 -0500

    refactor: put publishers data into src/data

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import PublisherList from './PublisherList'
 
+import publishers from '../data/publishers'
+
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      "publishers": {
-        "New York": [
-          "Akashic Books",
-          "Bellevue Literary Press",
-          "Catapult Books",
-          "Europa Editions",
-          "The Feminist Press",
-          "New Directions Publishing",
-          "Sevent Stories Press",
-          "Ugly Duckling Presse",
-        ],
-        "Columbus": [
-          "Two Dollar Radio",
-        ],
-      },
+      "publishers": publishers,
     };
   }
 
