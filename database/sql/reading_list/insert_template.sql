aad733613e514c5473539b17100bc0515fc4e64a
Kevin Nowland <kevin.nowland@gmail.com>
2022-12-31 14:39:17 -0500

    fix: remove purchase location type id from reading list insert template

--- database/sql/reading_list/insert_template.sql
+++ database/sql/reading_list/insert_template.sql
@@ -3,7 +3,6 @@ INSERT INTO reading_list
   book_id,
   stopped_reading_date,
   is_read_completely,
-  purchase_location_type_id,
   bookstore_id,
   website_id
 )
@@ -12,7 +11,6 @@ VALUES
   {book_id},
   {stopped_reading_date},
   {is_read_completely},
-  {purchase_location_type_id},
   {bookstore_id},
   {website_id}
 );
