db331fcb00a422259b05bf8a3145f341a9f8d6f2
Kevin Nowland <kevin.nowland@gmail.com>
2022-12-31 13:22:37 -0500

    refactor: change unique constraint on publisher

--- database/sql/publisher/create.sql
+++ database/sql/publisher/create.sql
@@ -1,8 +1,9 @@
 CREATE TABLE publisher (
     publisher_id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
     parent_name TEXT NOT NULL,
     city_id INTEGER NOT NULL,
     is_independent INTEGER NOT NULL,
-    FOREIGN KEY (city_id) REFERENCES city(city_id)
+    FOREIGN KEY (city_id) REFERENCES city(city_id),
+    UNIQUE(name, parent_name)
 );
