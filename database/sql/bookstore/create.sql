fbc4906f4da655806c5a0cb713c65caeda9e5102
Kevin Nowland <kevin.nowland@gmail.com>
2022-12-31 12:33:58 -0500

    fix: remove comma

--- database/sql/bookstore/create.sql
+++ database/sql/bookstore/create.sql
@@ -2,5 +2,5 @@ CREATE TABLE bookstore (
   bookstore_id INTEGER PRIMARY KEY,
   name TEXT NOT NULL UNIQUE,
   city_id INTEGER NOT NULL,
-  FOREIGN KEY (city_id) REFERENCES city(city_id),
+  FOREIGN KEY (city_id) REFERENCES city(city_id)
 );
