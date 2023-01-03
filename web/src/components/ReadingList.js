0466d992a88112093456cb3cbaaad7e326e9f456
Kevin Nowland <kevin.nowland@gmail.com>
2023-01-03 08:45:32 -0500

    feat: add publisher city to table

--- web/src/components/ReadingList.js
+++ web/src/components/ReadingList.js
@@ -2,6 +2,72 @@ import React from 'react';
 import _ from "lodash";
 import '../assets/ReadingList.css';
 
+function getCity(city) {
+  const stateAbbrevs = new Map([
+    ["Alabama", "AL"],
+    ["Alaska", "AK"],
+    ["Arizona", "AZ"],
+    ["Arkansa", "AR"],
+    ["California", "CA"],
+    ["Colorado", "CO"],
+    ["Connecticut", "CT"],
+    ["Delaware", "DE"],
+    ["D.C.", "DC"],
+    ["DC", "DC"],
+    ["District of Columbia", "DC"],
+    ["Florida", "FL"],
+    ["Georgia", "GA"],
+    ["Hawaii", "HI"],
+    ["Idaho", "ID"],
+    ["Illinois", "IL"],
+    ["Indiana", "IN"],
+    ["Iowa", "IA"],
+    ["Kansas", "KS"],
+    ["Kentucky", "KY"],
+    ["Louisiana", "LA"],
+    ["Maine", "ME"],
+    ["Maryland", "MD"],
+    ["Massachusetts", "MA"],
+    ["Michigan", "MI"],
+    ["Minnesota", "MN"],
+    ["Mississippi", "MS"],
+    ["Missouri", "MO"],
+    ["Montana", "MT"],
+    ["Nebraska", "NE"],
+    ["Nevada", "NV"],
+    ["New Hampshire", "NH"],
+    ["New Jersey", "NJ"],
+    ["New Mexico", "NM"],
+    ["New York", "NY"],
+    ["North Carolina", "NC"],
+    ["North Dakota", "ND"],
+    ["Ohio", "OH"],
+    ["Oklahoma", "OK"],
+    ["Oregon", "OR"],
+    ["Pennsylvania", "OR"],
+    ["Puerto Rico", "PR"],
+    ["Rhode Island", "RI"],
+    ["South Carolina", "SC"],
+    ["South Dakota", "SD"],
+    ["Tennessee", "TN"],
+    ["Texas", "TX"],
+    ["Utah", "UT"],
+    ["Vermont", "UT"],
+    ["Virgin Islands", "VI"],
+    ["Virginia", "VA"],
+    ["Washington", "WA"],
+    ["West Virginia", "WV"],
+    ["Wisconsin", "WI"],
+    ["Wyoming", "WY"]
+  ])
+
+  if (city.country === "United States") {
+    return city.city + ", " + stateAbbrevs.get(city.region)
+  } else {
+    return city.city + ", " + city.country
+  }
+}
+
 
 function getAuthors(authors) {
   var author_list = "";
@@ -64,6 +130,7 @@ function toTableRow(entry) {
       <td className="language center">{entry.book.language}</td>
       <td className="originaLanguage center">{getOriginalLanguage(entry.book.language, entry.book.original_language)}</td>
       <td className="publisher center">{entry.book.publisher.parent_name}</td>
+      <td className="publisherCity center">{getCity(entry.book.publisher.city)}</td>
       <td className="isIndie center">{checkOrNone(entry.book.publisher.is_independent)}</td>
       <td className="stoppedReadingDate center">{dateText(entry.stopped_reading_date)}</td>
       <td className="isReadCompletely center">{checkOrX(entry.is_read_completely)}</td>
@@ -106,8 +173,9 @@ class ReadingList extends React.Component {
               <th className="title">Title</th>
               <th className="authors">Author(s)</th>
               <th className="language">Language</th>
-              <th className="originalLanguage">Original Language</th>
+              <th className="originalLanguage">Orig. Language</th>
               <th className="publisher">Publisher</th>
+              <th className="publisherCity">Publisher City</th>
               <th className="isIndie">Indie?</th>
               <th className="stoppedReadingDate">Stopped Reading</th>
               <th className="isReadCompletely">Completed?</th>
