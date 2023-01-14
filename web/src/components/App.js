635f56e998520d54ceefc22db3e1910d628e6613
Kevin Nowland <kevin.nowland@gmail.com>
2023-01-14 13:27:13 -0500

    feat: remove publisher list from top of app

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import PublisherList from './PublisherList'
 import ReadingList from './ReadingList'
 
-import publishers from '../data/publishers'
 import reading_list from '../data/reading_list'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      "publishers": publishers,
       "readingList": reading_list,
     };
   }
@@ -17,18 +14,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <div>
-          {Object.entries(this.state.publishers).map(([k, v]) => {
-              return (
-                <div key={k} style={{display: 'block'}}>
-                <PublisherList key={k} city={k} publishers={v} />
-                </div>
-              );
-          })}
-        </div>
-
         <ReadingList readingList={this.state.readingList} />
-
         <div> <p> Shapefiles from census bureau data </p> </div>
       </div>
     )
