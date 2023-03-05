c5dc4df359a1053eca53c5da539d17146ea17ad6
Kevin Nowland <kevin.nowland@gmail.com>
2023-03-05 16:22:16 -0500

    feat: add initial ability to render cities

--- web/src/components/Map.js
+++ web/src/components/Map.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import * as d3 from 'd3';
-//import data from '../data/arizona'
-import data from '../data/usa_states_small.json'
+import _ from 'lodash';
+import cities from '../data/usa_cities.json'
+import states from '../data/usa_states_small.json'
 import '../assets/Map.css'
+
+
+const publisherCities = [
+  {"name": "New York City", "state": "New York"},
+  {"name": "Columbus", "state": "Ohio"},
+];
+
+function objectInList(obj, list) {
+  for (let i = 0; i < list.length; i++) {
+    if (_.isEqual(obj, list[i])) {
+      return true
+    }
+  }
+  return false
+}
+
+function getCitiesToRender() {
+  const features = cities.features.filter(f => objectInList(f.properties, publisherCities))
+  return {
+    "type": "FeatureCollection",
+    "features": features
+  }
+}
 
 
 function Map(props) {
@@ -10,9 +34,10 @@ function Map(props) {
   const height = 800;
   const projection = d3.geoAlbersUsa().scale(1500).translate([width / 2, height / 2]);
   const path = d3.geoPath().projection(projection);
+  const citiesToRender = getCitiesToRender();
 
-  const renderPaths = () => {
-    return data.features.map((d) => {
+  const renderStates = () => {
+    return states.features.map((d) => {
       return (
         <path
           key={d.properties.NAME}
@@ -26,9 +51,28 @@ function Map(props) {
     })
   };
 
+  const renderCities = () => {
+    return citiesToRender.features.map((d) => {
+      return (
+        <circle
+          key={d.properties.name + "-" + d.properties.state}
+          cx={projection(d.geometry.coordinates)[0]}
+          cy={projection(d.geometry.coordinates)[1]}
+          r='5'
+          fill="orange"
+          stroke='black'
+          strokeWidth='1'
+        />
+      )
+    })
+  };
+
   return (
     <div className='map'>
-      <svg height={height} width={width} className='map'>{renderPaths()}</svg>
+      <svg height={height} width={width} className='map'>
+        <g className="UsaStates">{renderStates()}</g>
+        <g className="UsaCities">{renderCities()}</g>
+      </svg>
     </div>
   )
 }
