d5798e52716b8e647df5ce31826fe3221bb271cb
Kevin Nowland <kevin.nowland@gmail.com>
2023-03-22 07:25:01 -0400

    refactor: move intro while working on other components

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,36 +1,37 @@
-import React from 'react';
-import ReadingList from './ReadingList'
-import Map from './Map'
-import Introduction from './Introduction'
+import React from "react";
+import ReadingList from "./ReadingList";
+import Map from "./Map";
+import Introduction from "./Introduction";
 
-import reading_list from '../data/reading_list'
-
+import reading_list from "../data/reading_list";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      "readingList": reading_list,
-      'introMarkdown': '',
+      readingList: reading_list,
+      introMarkdown: "",
     };
   }
 
   async componentDidMount() {
-    const res = await fetch(process.env.PUBLIC_URL + '/introduction.md')
-    const text = await res.text()
-    this.setState({ introMarkdown: text })
+    const res = await fetch(process.env.PUBLIC_URL + "/introduction.md");
+    const text = await res.text();
+    this.setState({ introMarkdown: text });
   }
 
   render() {
-
     return (
       <div>
-        <Introduction markdown={this.state.introMarkdown} />
         <Map />
         <ReadingList readingList={this.state.readingList} />
-        <div> <p> Shapefiles from census bureau data </p> </div>
+        <Introduction markdown={this.state.introMarkdown} />
+        <div>
+          {" "}
+          <p> Shapefiles from census bureau data </p>{" "}
+        </div>
       </div>
-    )
+    );
   }
 }
 
