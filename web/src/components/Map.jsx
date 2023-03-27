99a944af117657bbeeeb3298adab67e788b224f7
Kevin Nowland <kevin.nowland@gmail.com>
2023-03-26 20:58:42 -0400

    feat: change test for which cities to render

--- web/src/components/Map.jsx
+++ web/src/components/Map.jsx
@@ -1,38 +1,37 @@
-import React from 'react';
-import * as d3 from 'd3';
-import _ from 'lodash';
-import cities from '../data/usa_cities.json'
-import states from '../data/usa_states_small.json'
-import '../assets/Map.css'
+import React from "react";
+import * as d3 from "d3";
+import _ from "lodash";
+import cities from "../data/usa_cities.json";
+import states from "../data/usa_states_small.json";
+import publisherCities from "../data/publisher_cities_list.json";
+import "../assets/Map.css";
 
-
-const publisherCities = [
-  {"name": "New York City", "state": "New York"},
-  {"name": "Columbus", "state": "Ohio"},
-];
-
-function objectInList(obj, list) {
+function cityInList(obj, list) {
   for (let i = 0; i < list.length; i++) {
-    if (_.isEqual(obj, list[i])) {
-      return true
+    if (list[i].name === obj.name && list[i].state === obj.state) {
+      return true;
     }
   }
-  return false
+  return false;
 }
 
 function getCitiesToRender() {
-  const features = cities.features.filter(f => objectInList(f.properties, publisherCities))
+  const features = cities.features.filter((f) =>
+    cityInList(f.properties, publisherCities.cities)
+  );
   return {
-    "type": "FeatureCollection",
-    "features": features
-  }
+    type: "FeatureCollection",
+    features: features,
+  };
 }
-
 
 function Map(props) {
   const width = 1200;
   const height = 800;
-  const projection = d3.geoAlbersUsa().scale(1500).translate([width / 2, height / 2]);
+  const projection = d3
+    .geoAlbersUsa()
+    .scale(1500)
+    .translate([width / 2, height / 2]);
   const path = d3.geoPath().projection(projection);
   const citiesToRender = getCitiesToRender();
 
@@ -43,12 +42,12 @@ function Map(props) {
           key={d.properties.NAME}
           d={path(d)}
           className={d.properties.NAME}
-          fill='#F0F0F0'
-          strokeWidth='1'
-          stroke='black'
+          fill="#F0F0F0"
+          strokeWidth="1"
+          stroke="black"
         />
-      )
-    })
+      );
+    });
   };
 
   const renderCities = () => {
@@ -58,23 +57,23 @@ function Map(props) {
           key={d.properties.name + "-" + d.properties.state}
           cx={projection(d.geometry.coordinates)[0]}
           cy={projection(d.geometry.coordinates)[1]}
-          r='5'
+          r="5"
           fill="orange"
-          stroke='black'
-          strokeWidth='1'
+          stroke="black"
+          strokeWidth="1"
         />
-      )
-    })
+      );
+    });
   };
 
   return (
-    <div className='map'>
-      <svg height={height} width={width} className='map'>
+    <div className="map">
+      <svg height={height} width={width} className="map">
         <g className="UsaStates">{renderStates()}</g>
         <g className="UsaCities">{renderCities()}</g>
       </svg>
     </div>
-  )
+  );
 }
 
 export default Map;
