601dd59ed60090bba2a20f4c204cc983ab429b97
Kevin Nowland <kevin.nowland@gmail.com>
2023-04-29 15:49:59 -0400

    feat: add toggle between text and the reading list itself

--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReadingList from "./ReadingList";
 import Map from "./Map";
 import Markdown from "./Markdown";
@@ -13,13 +13,65 @@ import genreCounts from "../data/genreCounts";
 import languageCounts from "../data/languageCounts";
 import publisherCounts from "../data/publisherCounts";
 
+import "../assets/App.css";
+
+function Button(props) {
+  const text = props.text;
+  const isClicked = props.isClicked;
+  const onClick = props.onClick;
+
+  let style;
+  if (isClicked) {
+    style = { textDecoration: "underline" };
+  } else {
+    style = { textDecoration: "none" };
+  }
+
+  return (
+    <div className="button" onClick={onClick} style={style}>
+      {text}
+    </div>
+  );
+}
+
+function Nav(props) {
+  const showList = props.showList;
+  const updateShowList = props.updateShowList;
+
+  return (
+    <div className="nav">
+      <div className="buttons">
+        <Button
+          onClick={(e) => updateShowList(e, false)}
+          isClicked={!showList}
+          text="text"
+        />
+        <div className="separator"> · </div>
+        <Button
+          onClick={(e) => updateShowList(e, true)}
+          isClicked={showList}
+          text="reading list"
+        />
+      </div>
+    </div>
+  );
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      readingList: readingList,
+      showList: false,
       markdown: {},
     };
+
+    this.updateShowList = this.updateShowList.bind(this);
+  }
+
+  updateShowList(_, val) {
+    this.setState({
+      showList: val,
+    });
   }
 
   async componentDidMount() {
@@ -46,73 +98,95 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <Markdown markdown={this.state.markdown.intro} />
-        <DotChart
-          title={"Major vs Indie Publishers"}
-          data={publisherCounts}
-          width={800}
-          circlesPerRow={12}
-          colorShift={135}
-          colorFrequency={87}
+      <div className="app">
+        <Nav
+          updateShowList={this.updateShowList}
+          showList={this.state.showList}
         />
-        <Markdown markdown={this.state.markdown.indie} />
-        <Map />
-        <Markdown markdown={this.state.markdown.geography} />
-        <HorizontalBarChart />
-        <Markdown markdown={this.state.markdown.genre} />
-        <TriplePartitionChart
-          className="genre"
-          data={parseGenreCounts(genreCounts)}
-          keyOne="Fiction"
-          keyTwo="Non Fiction"
-          rootTitle="genre"
-          keyOneTitle="fiction"
-          keyTwoTitle="non fiction"
-          rootColor={{ hue: 30, saturation: 100, minLight: 5, maxLight: 22.5 }}
-          keyOneColor={{
-            hue: 240,
-            saturation: 89,
-            minLight: 27.5,
-            maxLight: 55,
-          }}
-          keyTwoColor={{
-            hue: 115,
-            saturation: 89,
-            minLight: 10,
-            maxLight: 25,
-          }}
-        />
-        <Markdown markdown={this.state.markdown.language} />
-        <TriplePartitionChart
-          className="language"
-          data={parseLanguageCounts(languageCounts)}
-          keyOne="English"
-          keyTwo="French"
-          rootTitle="language read"
-          keyOneTitle="english"
-          keyTwoTitle="french"
-          rootColor={{ hue: 30, saturation: 100, minLight: 5, maxLight: 22.5 }}
-          keyOneColor={{
-            hue: 240,
-            saturation: 89,
-            minLight: 27.5,
-            maxLight: 55,
-          }}
-          keyTwoColor={{
-            hue: 115,
-            saturation: 89,
-            minLight: 10,
-            maxLight: 25,
-          }}
-        />
-        <Markdown markdown={this.state.markdown.outro} />
-        <ReadingList readingList={this.state.readingList} />
-        <div>
-          {" "}
-          <p> Shapefiles from census bureau data </p>{" "}
+        <div
+          className="main"
+          style={{ display: this.state.showList ? "none" : "block" }}
+        >
+          <Markdown markdown={this.state.markdown.intro} />
+          <DotChart
+            title={"Major vs Indie Publishers"}
+            data={publisherCounts}
+            width={800}
+            circlesPerRow={12}
+            colorShift={135}
+            colorFrequency={87}
+          />
+          <Markdown markdown={this.state.markdown.indie} />
+          <Map />
+          <Markdown markdown={this.state.markdown.geography} />
+          <HorizontalBarChart />
+          <Markdown markdown={this.state.markdown.genre} />
+          <TriplePartitionChart
+            className="genre"
+            data={parseGenreCounts(genreCounts)}
+            keyOne="Fiction"
+            keyTwo="Non Fiction"
+            rootTitle="genre"
+            keyOneTitle="fiction"
+            keyTwoTitle="non fiction"
+            rootColor={{
+              hue: 30,
+              saturation: 100,
+              minLight: 5,
+              maxLight: 22.5,
+            }}
+            keyOneColor={{
+              hue: 240,
+              saturation: 89,
+              minLight: 27.5,
+              maxLight: 55,
+            }}
+            keyTwoColor={{
+              hue: 115,
+              saturation: 89,
+              minLight: 10,
+              maxLight: 25,
+            }}
+          />
+          <Markdown markdown={this.state.markdown.language} />
+          <TriplePartitionChart
+            className="language"
+            data={parseLanguageCounts(languageCounts)}
+            keyOne="English"
+            keyTwo="French"
+            rootTitle="language read"
+            keyOneTitle="english"
+            keyTwoTitle="french"
+            rootColor={{
+              hue: 30,
+              saturation: 100,
+              minLight: 5,
+              maxLight: 22.5,
+            }}
+            keyOneColor={{
+              hue: 240,
+              saturation: 89,
+              minLight: 27.5,
+              maxLight: 55,
+            }}
+            keyTwoColor={{
+              hue: 115,
+              saturation: 89,
+              minLight: 10,
+              maxLight: 25,
+            }}
+          />
+          <Markdown markdown={this.state.markdown.outro} />
+          <div>
+            {" "}
+            <p> Shapefiles from census bureau data </p>{" "}
+          </div>
         </div>
-      </div>
+        <ReadingList
+          readingList={readingList}
+          style={{ display: this.state.showList ? "block" : "none" }}
+        />
+      </div> // main
     );
   }
 }
